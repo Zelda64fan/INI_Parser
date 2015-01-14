049cd93dffccad27f6fb77d0c030bfbd7997968e
Zelda64fan <adam.armbrester@gmail.com>
2015-01-13 20:10:46 -0600

    Fixed case & terminology
    "Headers" are now "sections" to reflect the correct terminology and
    section & key names are no longer case sensitive.
    Fixed case & terminology
    Fixed terminology

--- INI_Parser.cpp
+++ INI_Parser.cpp
@@ -1,6 +1,8 @@
 // INI_Parser.h
 // Project:
-// Last updated: 2015-01-13 by Adam Armbrester: first version
+// Last updated: 2015-01-13 by Adam Armbrester: Changed "header" to "section" to
+// 	reflect correct terminology, made sections & keys be stored & processed in
+// 	lowercase so case won't matter anymore.
 // Implementation of INI_Parser.h
 
 #include "INI_Parser.h"
@@ -16,11 +18,11 @@ INI_Parser::~INI_Parser() {
 }
 
 char INI_Parser::ReadFile(std::string file) {
-	// If no headers are found, we return 2
-	bool found_headers = false;
+	// If no sections are found, we return 2
+	bool found_sections = false;
 
 	// Buffers for temporarily holding stuff
-	std::string header_buf = "";
+	std::string section_buf = "";
 	std::string key_buf = "";
 	std::string value_buf = "";
 	std::string line_buf = "";
@@ -40,17 +42,17 @@ char INI_Parser::ReadFile(std::string file) {
 		if (line_buf[0] == '#' || line_buf == "")
 			continue;
 		
-		if (line_buf[0] == '[') {    // Start of a new header
-			found_headers = true;
-			header_buf = line_buf;
+		if (line_buf[0] == '[') {    // Start of a new section
+			found_sections = true;
+			section_buf = line_buf;
 			// Remove the ] at the end; if it doesn't exist, the file is
 			// malformed
-			if (header_buf[header_buf.length() - 1] != ']')
+			if (section_buf[section_buf.length() - 1] != ']')
 				return INI_READ_MALFORMED;
 			else
-				header_buf = header_buf.substr(1, header_buf.length() - 2);
-		} else if (header_buf == "")   // Return if there's a key with no header
-			return INI_READ_KEY_WITH_NO_HEADER;
+				section_buf = section_buf.substr(1, section_buf.length() - 2);
+		} else if (section_buf == "")   // Return if there's a key with no section
+			return INI_READ_KEY_WITH_NO_SECTION;
 		else { // Start of a key
 			key_buf = "";
 			
@@ -73,39 +75,57 @@ char INI_Parser::ReadFile(std::string file) {
 				for (unsigned int i = valueStart + 1; i < line_buf.length(); i++)
 					value_buf += line_buf[i];
 
-			// If we made it this far, everything must be good
-			keymap[header_buf][key_buf] = value_buf;	
+			// If we made it this far, everything must be good. We'll convert to
+			// lowercase first, since these should not be case sensitive
+			std::transform(section_buf.begin(), section_buf.end(),
+				section_buf.begin(), ::tolower);
+			std::transform(key_buf.begin(), key.end(), key_buf.begin(),
+				::tolower);
+			keymap[section_buf][key_buf] = value_buf;	
 		}
 	}
 
-	if (!found_headers)
-		return INI_READ_NO_HEADERS;
+	if (!found_sections)
+		return INI_READ_NO_SECTIONS;
 	else
 		return INI_READ_SUCCESS;
 
 }
 
-bool INI_Parser::HeaderExists(std::string header) {
-	return (keymap.count(header) != 0);
+bool INI_Parser::SectionExists(std::string section) {
+	// Convert to lowercase, so case won't be an issue
+	std::transform(section.begin(), section.end(),
+		section.begin(), ::tolower);
+	return (keymap.count(section) != 0);
 }
 
-bool INI_Parser::KeyExists(std::string header, std::string key) {
-	if (!HeaderExists(header)) {
-		throw HeaderNotFoundException(header);
+bool INI_Parser::KeyExists(std::string section, std::string key) {
+	// Convert to lowercase
+	std::transform(section.begin(), section.end(),
+		section.begin(), ::tolower);
+	std::transform(key.begin(), key.end(), key.begin(), ::tolower);
+
+	if (!SectionExists(section)) {
+		throw SectionNotFoundException(section);
 		return false;
 	} else
-		return (keymap[header].count(key) != 0);
+		return (keymap[section].count(key) != 0);
 }
 
-std::string INI_Parser::GetKeyValue(std::string header, std::string key) {
-	if (!HeaderExists(header)) {
-		throw HeaderNotFoundException(header);
+std::string INI_Parser::GetKeyValue(std::string section, std::string key) {
+	// Convert to lowercase
+	std::transform(section.begin(), section.end(),
+		section.begin(), ::tolower);
+	std::transform(key.begin(), key.end(), key.begin(), ::tolower);
+
+	if (!SectionExists(section)) {
+		throw SectionNotFoundException(section);
 		return "";
-	} else if (!KeyExists(header, key)) {
-		throw KeyNotFoundException(header, key);
+	} else if (!KeyExists(section, key)) {
+		throw KeyNotFoundException(section, key);
 		return "";
 	} else
-		return keymap[header][key];
+		return keymap[section][key];
 }
 
 void INI_Parser::Unload() {
--- INI_Parser.h
+++ INI_Parser.h
@@ -1,24 +1,26 @@
 // INI_Parser.h
 // Project:
-// Last updated: 2015-01-13 by Adam Armbrester: first version
+// Last updated: 2015-01-13 by Adam Armbrester: Changed "header" to "section" to
+// 	reflect correct terminology, made sections & keys be stored & processed in
+// 	lowercase so case won't matter anymore.
 // Used to read and parse .ini files formatted like this:
-// 	[Header A]
+// 	[Section A]
 // 	key0=value0
 // 	key1=value1
-// 	# Lines starting with a # are comments
-// 	[Header B]
+// 	; Lines starting with a ; or # are comments
+// 	[Section B]
 // 	key0=value0
 // 	key1=value1
 // The contents of the file are read into two nested std::maps all at once. Key
 // values are returned as std::strings.
-// You can check that a header exists with HeaderExists(headerTitle), and check
-// keys with KeyExists(headerTitle, keyTitle). Key values can be read with
-// GetKeyValue(headerTitle, keyTitle).
+// You can check that a section exists with SectionExists(sectionTitle), and
+// check keys with KeyExists(sectionTitle, keyTitle). Key values can be read
+// with GetKeyValue(sectionTitle, keyTitle).
 // Errors:
-// 	HeaderNotFoundException: the specified header doesn't exist. Has a GetName()
-// 		function.
+// 	SectionNotFoundException: the specified section doesn't exist. Has a
+// 		GetName() function.
 // 	KeyNotFoundException: the specified key doesn't exist Not thrown if the
-// 		above is thrown. Has GetName() and GetHeaderName() functions.
+// 		above is thrown. Has GetName() and GetSectionName() functions.
 #include <map>
 #include <string>
 
@@ -28,8 +30,8 @@
 const char INI_READ_SUCCESS = 0;
 const char INI_READ_COULDNT_OPEN = 1;
 const char INI_READ_MALFORMED = 2;
-const char INI_READ_NO_HEADERS = 3;
-const char INI_READ_KEY_WITH_NO_HEADER = 4;
+const char INI_READ_NO_SECTIONS = 3;
+const char INI_READ_KEY_WITH_NO_SECTION = 4;
 
 class INI_Parser {
 public:
@@ -42,32 +44,32 @@ public:
 	/** Read the specified file into memory, so its contents can be processed.
 	 * Returns 0 on success and 1, 2, or 3 if something goes wrong (see
 	 * INI_READ_* consts for specific errors)
-	 * \return INI_READ_SUCCESS, INI_READ_NOT_FOUND, or INI_READ_NO_HEADERS,
+	 * \return INI_READ_SUCCESS, INI_READ_NOT_FOUND, or INI_READ_NO_SECTIONS,
 	 */
 	char ReadFile(std::string file);
 
-	/** Check if the specified header exists.
-	 * \param header The header to look for.
-	 * \return true if the header exists, false if it doesn't.
+	/** Check if the specified section exists.
+	 * \param section The section to look for.
+	 * \return true if the section exists, false if it doesn't.
 	 */
-	bool HeaderExists(std::string header);
+	bool SectionExists(std::string section);
 
-	/** Check if the specified key exists in the specified header. Will throw
-	 * HeaderNotFoundException if the header doesn't exist.
-	 * \param header The header to look in.
+	/** Check if the specified key exists in the specified section. Will throw
+	 * SectionNotFoundException if the section doesn't exist.
+	 * \param section The section to look in.
 	 * \param key The name of the key to look for.
-	 * \return true if the header exists, false if it doesn't.
+	 * \return true if the section exists, false if it doesn't.
 	 */
-	bool KeyExists(std::string header, std::string key);
+	bool KeyExists(std::string section, std::string key);
 
-	/** Get the value of a particular key from a particular header, as an
-	 * std::string. Will throw HeaderNotFoundException if the header doesn't
+	/** Get the value of a particular key from a particular section, as an
+	 * std::string. Will throw SectionNotFoundException if the section doesn't
 	 * exist, and KeyNotFoundException if the key doesn't.
-	 * \param header The header to look in.
+	 * \param section The section to look in.
 	 * \param key The name of the key whose value to get.
 	 * \return The key's value, as an std::string.
 	 */
-	std::string GetKeyValue(std::string header, std::string key);
+	std::string GetKeyValue(std::string section, std::string key);
 
 	/** Clears the file contents from memory. To access it again, the file must
 	 * be re-read with ReadFile().
@@ -75,19 +77,19 @@ public:
 	void Unload();
 
 private:
-	// Each key represents a header: its value is another map, whose keys are
-	// the keys for that header.
+	// Each key represents a section: its value is another map, whose keys are
+	// the keys for that section.
 	std::map<std::string, std::map<std::string, std::string> > keymap;
 };
 
-// Header not found
-class HeaderNotFoundException {
+// Section not found
+class SectionNotFoundException {
 public:
-	HeaderNotFoundException(std::string name) {
+	SectionNotFoundException(std::string name) {
 		_name = name;
 	}
 
-	~HeaderNotFoundException() {
+	~SectionNotFoundException() {
 		_name = "";
 	}
 
@@ -102,18 +104,18 @@ private:
 // Key not found
 class KeyNotFoundException {
 public:
-	KeyNotFoundException(std::string header, std::string name) {
-		_header = header;
+	KeyNotFoundException(std::string section, std::string name) {
+		_section = section;
 		_name = name;
 	}
 
 	~KeyNotFoundException() {
-		_header = "";
+		_section = "";
 		_name = "";
 	}
 
-	std::string GetHeaderName() {
-		return _header;
+	std::string GetSectionName() {
+		return _section;
 	}
 
 	std::string GetName() {
@@ -121,7 +123,7 @@ public:
 	}
 
 private:
-	std::string _header;
+	std::string _section;
 	std::string _name;
 };
 #endif
